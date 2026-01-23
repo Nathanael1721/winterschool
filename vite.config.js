b91df3325f57bf866d47049bc33b389be430dc72
Nathanael Tjahyadi <56219164+Nathanael1721@users.noreply.github.com>
2026-01-23 09:59:51 +0800

    update vite

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-    plugins: [cesium()]
+    plugins: [cesium()],
+    build: {
+        target: 'esnext'
+    }
 });
